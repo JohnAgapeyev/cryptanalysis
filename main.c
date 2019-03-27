3df7626201c5c1a4325715bcc937ddfe326cc733
John Agapeyev <jagapeyev@gmail.com>
2019-03-26 17:05:12 -0700

    Got rotation actually working

--- main.c
+++ main.c
@@ -1,7 +1,37 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stddef.h>
+#include <stdint.h>
+
+#include "util.h"
 
 int main(int argc, char **argv) {
+    uint8_t x = 0xab;
+
+    uint8_t y = ROTL(x, 3);
+    uint8_t z = ROTR(y, 3);
+
+    printf("Input %d, Output %d, Again %d\n", x, y, z);
+
+    uint16_t a = 0xabcd;
+
+    uint16_t b = ROTL(a, 3);
+    uint16_t c = ROTR(b, 3);
+
+    printf("Input %d, Output %d, Again %d\n", a, b, c);
+    uint32_t d = 0x00010203;
+
+    uint32_t e = ROTL(d, 3);
+    uint32_t f = ROTR(e, 3);
+
+    printf("Input %d, Output %d, Again %d\n", d, e, f);
+
+    uint64_t g = 0x0001020304050607;
+
+    uint64_t h = ROTL(g, 3);
+    uint64_t i = ROTR(h, 3);
+
+    printf("Input %lu, Output %lu, Again %lu\n", g, h, i);
+
     return EXIT_SUCCESS;
 }
--- util.h
+++ util.h
@@ -2,10 +2,16 @@
 #define UTIL_H
 
 #include <limits.h>
+#include <stdint.h>
 
-#define ROTL(a,b) (((a) << (b)) | ((a) >> (sizeof(a) * CHAR_BIT - (b))))
-#define ROTR(a,b) (((a) >> (b)) | ((a) << (sizeof(a) * CHAR_BIT - (b))))
+#define ROTL(a, b) (((a) << (b)) | ((a) >> (sizeof(a) * CHAR_BIT - (b))))
+#define ROTR(a, b) (((a) >> (b)) | ((a) << (sizeof(a) * CHAR_BIT - (b))))
 
-void foo(void);
+typedef union {
+    uint64_t qword;
+    uint32_t dword[2];
+    uint16_t word[4];
+    uint8_t byte[8];
+} DATA;
 
 #endif
