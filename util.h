c0e5c2cbee73851e056c007253db4cf346f44975
John Agapeyev <jagapeyev@gmail.com>
2019-03-26 17:13:43 -0700

    Added variants for larger data sizes

--- util.h
+++ util.h
@@ -12,6 +12,20 @@ typedef union {
     uint32_t dword[2];
     uint16_t word[4];
     uint8_t byte[8];
-} DATA;
+} DATA_64;
+
+typedef union {
+    uint64_t qword[2];
+    uint32_t dword[4];
+    uint16_t word[8];
+    uint8_t byte[16];
+} DATA_128;
+
+typedef union {
+    uint64_t qword[4];
+    uint32_t dword[8];
+    uint16_t word[16];
+    uint8_t byte[32];
+} DATA_256;
 
 #endif
